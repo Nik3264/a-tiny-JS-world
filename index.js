fabf65e784278ed3e78d1d6110c9ab3196050fa3
Oksana <example@gmail.com>
2022-09-07 13:16:50 +0300

    creating lines for output by arr.map

--- index.js
+++ index.js
@@ -7,8 +7,55 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+const dog = {
+   species: 'dog',
+   name: 'Toby',
+   gender: 'male',
+   legs: 4,
+   hands: 0,
+   saying: 'woof-woof!'
+ };
 
+ const cat = {
+   species: 'cat',
+   name: 'Vasya',
+   gender: 'male',
+   legs: 4,
+   hands: 0,
+   saying: 'mi-mi!'
+ };
+ const catwoman = {
+   species: 'catwoman',
+   name: 'Toby',
+   gender: 'male',
+   legs: 4,
+   hands: 0,
+   saying: cat.saying
+ };
+ const men = {
+   species: 'men',
+   name: 'Toby',
+   gender: 'male',
+   legs: 2,
+   hands: 2,
+   saying: 'hello!'
+ };
+ const woman = {
+   species: 'woman',
+   name: 'Tyna',
+   gender: 'female',
+   legs: 2,
+   hands: 2,
+   saying: 'wo hei!'
+ };
 
+ const arr = [dog, cat, catwoman, men, woman];
+
+ const str = arr.map(el=>{
+  el.name=`<strong>${el.name}</strong>`;
+   return Object.values(el).join(', ');
+ });
+ //console.log(str.join(";  "));
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
@@ -26,6 +73,6 @@
    print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
+*/
+print(str.join(";\n"));
 
